e9c7f9fb5271c61b51b4f230930de05f6accc9db
falco <gyrovorbis@gmail.com>
2022-07-07 02:12:14 -0400

    Removed test doxygen comments from GblType

--- lib/api/gimbal/meta/gimbal_type.h
+++ lib/api/gimbal/meta/gimbal_type.h
@@ -1,16 +1,3 @@
-/**
- * @file gimbal_type.h
- * @author GyroVorbis
- * @date 9 Sep 2012
- * @brief File containing example of doxygen usage for quick reference.
- *
- * Here typically goes a more extensive explanation of what the header
- * defines. Doxygens tags are words preceeded by either a backslash @\
- * or by an at symbol @@.
- * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
- * @see http://www.stack.nl/~dimitri/doxygen/commands.html
- */
-
 #ifndef GIMBAL_TYPE_H
 #define GIMBAL_TYPE_H
 
