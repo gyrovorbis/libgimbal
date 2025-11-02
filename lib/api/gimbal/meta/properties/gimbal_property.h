ded96821a02d51326029b62ecdb71ce3d9684b13
Falco Girgis <gyrovorbis@gmail.com>
2025-11-01 22:44:04 -0500

    Documented the shit outta gimbal_property.h.
    1) gimbal_property.h
        - Completed Doxygen documentation.
        - Organized everything into Doxygen subgroups.
        - Went full autist on formatting and beautification.

--- lib/api/gimbal/meta/properties/gimbal_property.h
+++ lib/api/gimbal/meta/properties/gimbal_property.h
@@ -6,7 +6,10 @@
  *  for GblProperty, the root property instance from
  *  which all other properties are derived.
  *
- *  \author     2023 Falco Girgis
+ *  Properties are string-keyed values of any data type
+ *  which can be added to a GblObject.
+ *
+ *  \author     2023, 2025 Falco Girgis
  *  \copyright  MIT License
  */
 #ifndef GIMBAL_PROPERTY_H
@@ -33,7 +36,7 @@
  */
 //! Declares a list of properties for the given object/instance structure
 #define GBL_PROPERTIES(object, ...)              GBL_PROPERTIES_(object, __VA_ARGS__)
-//! Registeres the list of properties which were declared with GBL_PROPERTIES()
+//! Registers the list of properties which were declared with GBL_PROPERTIES()
 #define GBL_PROPERTIES_REGISTER(/*object,*/ ...) GBL_PROPERTIES_REGISTER_(__VA_ARGS__)
 //! @}
 
@@ -43,23 +46,23 @@ GBL_DECLS_BEGIN
 
 GBL_FORWARD_DECLARE_STRUCT(GblProperty);
 
-//! Function signature used as an iterator with GblProperty_foreach()
+//! Function signature used as an iterator with GblProperty_foreach(), returns GBL_FALSE when iteration should cease.
 typedef GblBool (*GblPropertyIterFn)(const GblProperty* pProp, void* pClosure);
 
 /*! \enum  GBL_PROPERTY_FLAG
  *  \brief Flags used to denote property attributes
  */
 GBL_DECLARE_ENUM(GBL_PROPERTY_FLAG) {
-    GBL_PROPERTY_FLAG_CONSTRUCT     = 0x1,   //!< Property must be given to the constructor
-    GBL_PROPERTY_FLAG_READ          = 0x2,   //!< Property value can be read
-    GBL_PROPERTY_FLAG_WRITE         = 0x4,   //!< Property value can be modified
-    GBL_PROPERTY_FLAG_SAVE          = 0x8,   //!< Property is serialized when saving
-    GBL_PROPERTY_FLAG_LOAD          = 0x10,  //!< Property is deserialized when loading
-    GBL_PROPERTY_FLAG_ABSTRACT      = 0x20,  //!< Property must be implemented by deriving type
-    GBL_PROPERTY_FLAG_OVERRIDE      = 0x40,  //!< Property overrides an existing property
-    GBL_PROPERTY_FLAG_READ_WRITE    = 0x6,   //!< Property is both readable and writable
-    GBL_PROPERTY_FLAG_SAVE_LOAD     = 0x18,  //!< Property is both savable and loadable
-    GBL_PROPERTY_FLAG_ALL           = 0xffff //!< Mask for all property flags
+    GBL_PROPERTY_FLAG_CONSTRUCT     = 0x1,   //!< Property must be given to the constructor.
+    GBL_PROPERTY_FLAG_READ          = 0x2,   //!< Property value can be read.
+    GBL_PROPERTY_FLAG_WRITE         = 0x4,   //!< Property value can be modified.
+    GBL_PROPERTY_FLAG_SAVE          = 0x8,   //!< Property is serialized when saving.
+    GBL_PROPERTY_FLAG_LOAD          = 0x10,  //!< Property is deserialized when loading.
+    GBL_PROPERTY_FLAG_ABSTRACT      = 0x20,  //!< Property must be implemented by deriving type.
+    GBL_PROPERTY_FLAG_OVERRIDE      = 0x40,  //!< Property overrides an existing property.
+    GBL_PROPERTY_FLAG_READ_WRITE    = 0x6,   //!< Property is both readable and writable.
+    GBL_PROPERTY_FLAG_SAVE_LOAD     = 0x18,  //!< Property is both savable and loadable.
+    GBL_PROPERTY_FLAG_ALL           = 0xffff //!< Mask for all property flags.
 };
 
 /*! \struct  GblPropertyClass
@@ -103,92 +106,142 @@ GBL_CLASS_END
  */
 GBL_INSTANCE_DERIVE(GblProperty, GblBox)
     GBL_PRIVATE_BEGIN
-        GblProperty* pNext;
-        GblType      objectType;
+        GblProperty* pNext;      //!< Pointer to next property contained by the same type.
+        GblType      objectType; //!< UUID of the owning type.
     GBL_PRIVATE_END
-    GblQuark    name;
-    size_t      id;
-    GblFlags    flags;
-    GblType     valueType;
+    GblQuark    name;            //!< Interned string name of property.
+    size_t      id;              //!< Integer ID of property.
+    GblFlags    flags;           //!< Combined flags of property.
+    GblType     valueType;       //!< Type of the value represented by the property.
 GBL_INSTANCE_END
 
-GBL_EXPORT GblType      GblProperty_type           (void)                                  GBL_NOEXCEPT;
-GBL_EXPORT size_t       GblProperty_totalCount     (void)                                  GBL_NOEXCEPT;
+//! Returns the UUID associated with the GblProperty type, registering it if it hasn't been already.
+GBL_EXPORT GblType GblProperty_type (void) GBL_NOEXCEPT;
 
-GBL_EXPORT GBL_RESULT   GblProperty_install        (GblType objType, GblProperty* pProp)   GBL_NOEXCEPT;
-GBL_EXPORT GblBool      GblProperty_uninstall      (GblType objType, const char* pName)    GBL_NOEXCEPT;
-GBL_EXPORT GblBool      GblProperty_uninstallQuark (GblType objType, GblQuark name)        GBL_NOEXCEPT;
-GBL_EXPORT GblBool      GblProperty_uninstallAll   (GblType objType)                       GBL_NOEXCEPT;
+/*! \name  Management
+    \brief Routines for installing and uninstalling properties.
+    @{
+*/
+//! Installs the given property onto the given type (which must be GblObject compatible).
+GBL_EXPORT GBL_RESULT GblProperty_install        (GblType objType, GblProperty* pProp) GBL_NOEXCEPT;
+//! Uninstalls the property with the given name from the given object type.
+GBL_EXPORT GblBool    GblProperty_uninstall      (GblType objType, const char* pName)  GBL_NOEXCEPT;
+//! Uninstalls the property with the given name quark from the given object type.
+GBL_EXPORT GblBool    GblProperty_uninstallQuark (GblType objType, GblQuark name)      GBL_NOEXCEPT;
+//! Uninstalls all properties which were previously installed onto the given object type.
+GBL_EXPORT GblBool    GblProperty_uninstallAll   (GblType objType)                     GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT size_t       GblProperty_count          (GblType objectType)                    GBL_NOEXCEPT;
-GBL_EXPORT GblFlags     GblProperty_combinedFlags  (GblType objectType)                    GBL_NOEXCEPT;
+/*! \name  Info
+    \brief Querying for miscellaneous property info.
+    @{
+*/
+//! Returns the total number of properties which have been registered onto any GblType.
+GBL_EXPORT size_t   GblProperty_totalCount    (void)               GBL_NOEXCEPT;
+//! Returns the number of properties which were explicitly registered to the given type (not including inherited properties).
+GBL_EXPORT size_t   GblProperty_count         (GblType objectType) GBL_NOEXCEPT;
+//! Returns the combined, bitwise OR'd flags from all properties registered onto the given type.
+GBL_EXPORT GblFlags GblProperty_combinedFlags (GblType objectType) GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT const GblProperty*
-                        GblProperty_find           (GblType objectType, const char* pName) GBL_NOEXCEPT;
-GBL_EXPORT const GblProperty*
-                        GblProperty_findQuark      (GblType objectType, GblQuark name)     GBL_NOEXCEPT;
-GBL_EXPORT const GblProperty*
-                        GblProperty_next           (GblType            objectType,
-                                                    const GblProperty* pPrev,
-                                                    GblFlags           mask)               GBL_NOEXCEPT;
+/*! \name  Querying
+    \brief Routines for looking up installed properties.
+    @{
+*/
+//! Looks up the given property by string name which was previously installed onto the given GblObject type.
+GBL_EXPORT const GblProperty* GblProperty_find      (GblType objectType, const char* pName) GBL_NOEXCEPT;
+//! Looks up the given property by GblQuark name which was previously installed onto the given GblObject type.
+GBL_EXPORT const GblProperty* GblProperty_findQuark (GblType objectType, GblQuark name)     GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT GblBool      GblProperty_foreach        (GblType            objectType,
-                                                    GBL_PROPERTY_FLAG  flags,
-                                                    GblPropertyIterFn  pFnIt,
-                                                    void*              pClosure)           GBL_NOEXCEPT;
-// ===== INSTANCE =====
+/*! \name  Iterating
+    \brief Routines for iterating over installed properties.
+    @{
+*/
+//! Returns the next property installed onto the given type, given the current property (or NULL for the first) and the flag mask for which properties to include.
+GBL_EXPORT const GblProperty* GblProperty_next    (GblType            objectType,
+                                                   const GblProperty* pPrev,
+                                                   GblFlags           mask)     GBL_NOEXCEPT;
+//! Iterates over the properties installed onto the given type containing any of the given flags, calling the given callback, which gets passed back an optional closure. Returns false if the callback returns false to end iteration.
+GBL_EXPORT GblBool            GblProperty_foreach (GblType            objectType,
+                                                   GBL_PROPERTY_FLAG  flags,
+                                                   GblPropertyIterFn  pFnIt,
+                                                   void*              pClosure) GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT GblProperty* GblProperty_create         (GblType     derivedType,
-                                                    const char* pName,
-                                                    size_t      id,
-                                                    GblFlags    flags,
-                                                    size_t      optionalArgCount,
-                                                    ...)                                   GBL_NOEXCEPT;
+/*! \name  Initializing
+    \brief Routines for creating and constructing new properties.
+    @{
+*/
+//! Creates a new property on the heap with the given type, name, id, flags, and any optional arguments handled by its optional argument initializer.
+GBL_EXPORT GblProperty* GblProperty_create          (GblType     derivedType,
+                                                     const char* pName,
+                                                     size_t      id,
+                                                     GblFlags    flags,
+                                                     size_t      optionalArgCount,
+                                                     ...)                            GBL_NOEXCEPT;
+//! Equivalent to GblProperty_create(), except taking a va_list rather than variadic argument list.
+GBL_EXPORT GblProperty* GblProperty_createVaList    (GblType     derivedType,
+                                                     const char* pName,
+                                                     size_t      id,
+                                                     GblFlags    flags,
+                                                     size_t      optionalArgCount,
+                                                     va_list*    pList)              GBL_NOEXCEPT;
+//! Constructs a preallocated property with the given type, name, id, flags, and any optional arguments handled by its optional argument initializer.
+GBL_EXPORT GBL_RESULT   GblProperty_construct       (GBL_SELF,
+                                                     GblType     derivedType,
+                                                     const char* pName,
+                                                     size_t      id,
+                                                     GblFlags    flags,
+                                                     size_t      optionalArgCount,
+                                                     ...)                            GBL_NOEXCEPT;
+//! Equivalent to GblProperty_construct(), except taking a va_list rather than variadic argument list.
+GBL_EXPORT GBL_RESULT   GblProperty_constructVaList (GBL_SELF,
+                                                     GblType     derivedType,
+                                                     const char* pName,
+                                                     size_t      id,
+                                                     GblFlags    flags,
+                                                     size_t      optionalArgCount,
+                                                     va_list*    pList)              GBL_NOEXCEPT;
+//! Either calls GblProperty_create() or GblProperty_construct() based on whether ppSelf points to an existing property or not. If not, it will point to a newly created property, upon success, after the function returns.
+GBL_EXPORT GBL_RESULT GblProperty_createOrConstruct (GblProperty** ppSelf,
+                                                     GblType       derivedType,
+                                                     const char*   pName,
+                                                     size_t        id,
+                                                     GblFlags      flags,
+                                                     size_t        optionalArgCount,
+                                                     ...)                            GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT GblProperty* GblProperty_createVaList   (GblType     derivedType,
-                                                    const char* pName,
-                                                    size_t      id,
-                                                    GblFlags    flags,
-                                                    size_t      optionalArgCount,
-                                                    va_list*    pList)                     GBL_NOEXCEPT;
+/*! \name  Accessors
+    \brief Routines for accessing internal property data.
+    @{
+*/
+//! Returns the UUID of the type associated with the given property.
+GBL_EXPORT GblType     GblProperty_objectType (GBL_CSELF) GBL_NOEXCEPT;
+//! Returns the string name of the given property.
+GBL_EXPORT const char* GblProperty_nameString (GBL_CSELF) GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT GBL_RESULT   GblProperty_construct      (GBL_SELF,
-                                                    GblType     derivedType,
-                                                    const char* pName,
-                                                    size_t      id,
-                                                    GblFlags    flags,
-                                                    size_t      optionalArgCount,
-                                                    ...)                                   GBL_NOEXCEPT;
+/*! \name  Methods
+    \brief Routines for calling class methods on properties.
+    @{
+*/
+//! Sets the given variant to contain the default value of the given property, calling GblPropertyClass::pFnDefaultValue().
+GBL_EXPORT GBL_RESULT GblProperty_defaultValue  (GBL_CSELF, GblVariant* pValue)       GBL_NOEXCEPT;
+//! Checks whether the value contained by the given variant is valid for the given property, calling GblPropertyClass::pFnCeckValue().
+GBL_EXPORT GblBool    GblProperty_checkValue    (GBL_CSELF, const GblVariant* pValue) GBL_NOEXCEPT;
+//! Modifies the value contained within the given variant to be a valid value, if possible, calling GblPropertyClass::pFnValidateValue().
+GBL_EXPORT GBL_RESULT GblProperty_validateValue (GBL_CSELF, GblVariant* pValue)       GBL_NOEXCEPT;
+//! Compares the values contained by the given variants, based on the rules of the given property, calling GblPropertyClass::pFnCompareValues().
+GBL_EXPORT int        GblProperty_compareValues (GBL_CSELF,
+                                                 const GblVariant* pV1,
+                                                 const GblVariant* pV2)               GBL_NOEXCEPT;
+//! @}
 
-GBL_EXPORT GBL_RESULT   GblProperty_constructVaList(GBL_SELF,
-                                                    GblType     derivedType,
-                                                    const char* pName,
-                                                    size_t      id,
-                                                    GblFlags    flags,
-                                                    size_t      optionalArgCount,
-                                                    va_list*    pList)                     GBL_NOEXCEPT;
-
-GBL_EXPORT GBL_RESULT GblProperty_createOrConstruct(GblProperty** ppSelf,
-                                                    GblType       derivedType,
-                                                    const char*   pName,
-                                                    size_t        id,
-                                                    GblFlags      flags,
-                                                    size_t        optionalArgCount,
-                                                    ...)                                   GBL_NOEXCEPT;
-
-GBL_EXPORT GblType      GblProperty_objectType     (GBL_CSELF)                             GBL_NOEXCEPT;
-GBL_EXPORT const char*  GblProperty_nameString     (GBL_CSELF)                             GBL_NOEXCEPT;
-
-GBL_EXPORT GBL_RESULT   GblProperty_defaultValue   (GBL_CSELF, GblVariant* pValue)         GBL_NOEXCEPT;
-GBL_EXPORT GblBool      GblProperty_checkValue     (GBL_CSELF, const GblVariant* pValue)   GBL_NOEXCEPT;
-GBL_EXPORT GBL_RESULT   GblProperty_validateValue  (GBL_CSELF, GblVariant* pValue)         GBL_NOEXCEPT;
-
-GBL_EXPORT int          GblProperty_compareValues  (GBL_CSELF,
-                                                    const GblVariant* pV1,
-                                                    const GblVariant* pV2)                 GBL_NOEXCEPT;
 GBL_DECLS_END
 
-//! \cond
+//! \cond PRIVATE MACRO IMPLEMENTATIONS
 #define GBL_PROPERTIES_(object, ...)                        \
     GBL_PROPERTIES_IDS_(object, __VA_ARGS__)                \
     GBL_PROPERTIES_REGISTER_DEFINE_(object, __VA_ARGS__)
