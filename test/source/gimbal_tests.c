5f0ed7b87edb1e7a93bb7dc90b916ef006522779
Haikuno <mopprower@gmail.com>
2026-07-01 20:54:12 -0300

    tidy things up

--- test/source/gimbal_tests.c
+++ test/source/gimbal_tests.c
@@ -1,58 +1,64 @@
-#include <gimbal/test/gimbal_test_suite.h>
-#include "gimbal_tests.h"
-#include "algorithms/gimbal_sort_test_suite.h"
+#include "algorithms/gimbal_compression_test_suite.h"
 #include "algorithms/gimbal_hash_test_suite.h"
-#include "algorithms/gimbal_sha1_test_suite.h"
 #include "algorithms/gimbal_md5_test_suite.h"
-#include "containers/gimbal_linked_list_test_suite.h"
-#include "containers/gimbal_doubly_linked_list_test_suite.h"
-#include "containers/gimbal_nary_tree_test_suite.h"
-#include "containers/gimbal_array_list_test_suite.h"
-#include "containers/gimbal_ring_list_test_suite.h"
-#include "containers/gimbal_ring_buffer_test_suite.h"
-#include "containers/gimbal_array_map_test_suite.h"
-#include "containers/gimbal_tree_set_test_suite.h"
-#include "containers/gimbal_hash_set_test_suite.h"
-#include "containers/gimbal_array_deque_test_suite.h"
-#include "containers/gimbal_array_heap_test_suite.h"
+#include "algorithms/gimbal_random_test_suite.h"
+#include "algorithms/gimbal_sha1_test_suite.h"
+#include "algorithms/gimbal_sort_test_suite.h"
+
 #include "allocators/gimbal_arena_allocator_test_suite.h"
 #include "allocators/gimbal_pool_allocator_test_suite.h"
 #include "allocators/gimbal_scope_allocator_test_suite.h"
-#include "utils/gimbal_ref_test_suite.h"
-#include "utils/gimbal_byte_array_test_suite.h"
-#include "strings/gimbal_quark_test_suite.h"
-#include "strings/gimbal_string_view_test_suite.h"
-#include "strings/gimbal_string_ref_test_suite.h"
-#include "strings/gimbal_string_buffer_test_suite.h"
-#include "strings/gimbal_string_list_test_suite.h"
-#include "strings/gimbal_pattern_test_suite.h"
-#include "meta/types/gimbal_type_test_suite.h"
+
+#include "containers/gimbal_array_deque_test_suite.h"
+#include "containers/gimbal_array_heap_test_suite.h"
+#include "containers/gimbal_array_list_test_suite.h"
+#include "containers/gimbal_array_map_test_suite.h"
+#include "containers/gimbal_doubly_linked_list_test_suite.h"
+#include "containers/gimbal_hash_set_test_suite.h"
+#include "containers/gimbal_linked_list_test_suite.h"
+#include "containers/gimbal_nary_tree_test_suite.h"
+#include "containers/gimbal_ring_buffer_test_suite.h"
+#include "containers/gimbal_ring_list_test_suite.h"
+#include "containers/gimbal_tree_set_test_suite.h"
+
+#include "core/gimbal_error_test_suite.h"
+#include "core/gimbal_exception_test_suite.h"
+#include "core/gimbal_module_test_suite.h"
+#include "core/gimbal_thread_test_suite.h"
+
 #include "meta/classes/gimbal_class_test_suite.h"
-#include "meta/ifaces/gimbal_interface_test_suite.h"
-#include "meta/instances/gimbal_instance_test_suite.h"
-#include "meta/signals/gimbal_closure_test_suite.h"
-#include "meta/signals/gimbal_signal_test_suite.h"
-#include "meta/types/gimbal_variant_test_suite.h"
 #include "meta/classes/gimbal_enum_test_suite.h"
 #include "meta/classes/gimbal_flags_test_suite.h"
 #include "meta/classes/gimbal_opaque_test_suite.h"
-#include "meta/types/gimbal_pointer_test_suite.h"
-#include "meta/instances/gimbal_box_test_suite.h"
-#include "meta/instances/gimbal_object_test_suite.h"
+#include "meta/ifaces/gimbal_interface_test_suite.h"
 #include "meta/ifaces/gimbal_itable_variant_test_suite.h"
-#include "utils/gimbal_uuid_test_suite.h"
-#include "utils/gimbal_version_test_suite.h"
-#include "utils/gimbal_option_group_test_suite.h"
+#include "meta/instances/gimbal_box_test_suite.h"
+#include "meta/instances/gimbal_instance_test_suite.h"
+#include "meta/instances/gimbal_object_test_suite.h"
+#include "meta/signals/gimbal_closure_test_suite.h"
+#include "meta/signals/gimbal_signal_test_suite.h"
+#include "meta/types/gimbal_pointer_test_suite.h"
+#include "meta/types/gimbal_type_test_suite.h"
+#include "meta/types/gimbal_variant_test_suite.h"
+
+#include "strings/gimbal_pattern_test_suite.h"
+#include "strings/gimbal_quark_test_suite.h"
+#include "strings/gimbal_string_buffer_test_suite.h"
+#include "strings/gimbal_string_list_test_suite.h"
+#include "strings/gimbal_string_ref_test_suite.h"
+#include "strings/gimbal_string_view_test_suite.h"
+
+#include "utils/gimbal_bit_view_test_suite.h"
+#include "utils/gimbal_byte_array_test_suite.h"
 #include "utils/gimbal_cmd_parser_test_suite.h"
 #include "utils/gimbal_date_time_test_suite.h"
-#include "utils/gimbal_bit_view_test_suite.h"
-#include "core/gimbal_module_test_suite.h"
-#include "core/gimbal_thread_test_suite.h"
+#include "utils/gimbal_option_group_test_suite.h"
+#include "utils/gimbal_ref_test_suite.h"
 #include "utils/gimbal_scanner_test_suite.h"
-#include "algorithms/gimbal_random_test_suite.h"
-#include "algorithms/gimbal_compression_test_suite.h"
-#include "core/gimbal_exception_test_suite.h"
-#include "core/gimbal_error_test_suite.h"
+#include "utils/gimbal_uuid_test_suite.h"
+#include "utils/gimbal_version_test_suite.h"
+
+#include "gimbal_tests.h"
 
 #ifdef GBL_ENABLE_CPP
 #   include "strings/gimbal_quark_test_suite.hpp"
@@ -131,7 +137,6 @@ int start_logger(const char *app_name)
 
 #endif
 
-
 static GBL_RESULT GimbalTests_constructed_(GblObject* pSelf) {
     GBL_CTX_BEGIN(NULL);
 
@@ -145,125 +150,126 @@ static GBL_RESULT GimbalTests_constructed_(GblObject* pSelf) {
                                                     GBL_LOG_LEVEL_ERROR);
 
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_SORT_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_HASH_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_SHA1_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_MD5_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_LINKED_LIST_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_DOUBLY_LINKED_LIST_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_NARY_TREE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ARRAY_LIST_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_RING_LIST_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_RING_BUFFER_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ARRAY_MAP_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_TREE_SET_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_HASH_SET_TEST_SUITE_TYPE));
+                                 GblTestSuite_create(GBL_ARENA_ALLOCATOR_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_ARRAY_DEQUE_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_ARRAY_HEAP_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ARENA_ALLOCATOR_TEST_SUITE_TYPE));
+                                 GblTestSuite_create(GBL_ARRAY_LIST_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_POOL_ALLOCATOR_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_SCOPE_ALLOCATOR_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_REF_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_BYTE_ARRAY_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_UUID_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_QUARK_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_STRING_VIEW_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_STRING_REF_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_STRING_BUFFER_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_PATTERN_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_STRING_LIST_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_TYPE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_CLASS_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_INTERFACE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_INSTANCE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_VARIANT_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_BOX_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ITABLE_VARIANT_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_CLOSURE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_SIGNAL_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ENUM_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_FLAGS_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_POINTER_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_OPAQUE_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_OBJECT_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_VERSION_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_OPTION_GROUP_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_CMD_PARSER_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_DATE_TIME_TEST_SUITE_TYPE));
+                                 GblTestSuite_create(GBL_ARRAY_MAP_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_BIT_VIEW_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_SCANNER_TEST_SUITE_TYPE));
+                                 GblTestSuite_create(GBL_BOX_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_BYTE_ARRAY_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_CLASS_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_CLOSURE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_CMD_PARSER_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_COMPRESSION_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_DATE_TIME_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_DOUBLY_LINKED_LIST_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_ENUM_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_ERROR_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_EXCEPTION_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_FLAGS_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_HASH_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_HASH_SET_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_INSTANCE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_INTERFACE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_ITABLE_VARIANT_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_LINKED_LIST_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_MD5_TEST_SUITE_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_MODULE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_NARY_TREE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_OBJECT_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_OPAQUE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_OPTION_GROUP_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_PATTERN_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_POINTER_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_POOL_ALLOCATOR_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_QUARK_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_RANDOM_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_REF_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_RING_BUFFER_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_RING_LIST_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_SCANNER_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_SCOPE_ALLOCATOR_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_SHA1_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_SIGNAL_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_SORT_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_STRING_BUFFER_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_STRING_LIST_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_STRING_REF_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_STRING_VIEW_TEST_SUITE_TYPE));
 #if GBL_THREADS_ENABLED
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_THREAD_TEST_SUITE_TYPE));
 #endif
-#ifdef GBL_ENABLE_CPP
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_QUARK_TEST_SUITE_CPP_TYPE));
+                                 GblTestSuite_create(GBL_TREE_SET_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_TYPE_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_UUID_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_VARIANT_TEST_SUITE_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_VERSION_TEST_SUITE_TYPE));
+
+#ifdef GBL_ENABLE_CPP
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_PATTERN_TEST_SUITE_CPP_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_STRING_VIEW_TEST_SUITE_CPP_TYPE));
+                                 GblTestSuite_create(GBL_QUARK_TEST_SUITE_CPP_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_STRING_REF_TEST_SUITE_CPP_TYPE));
     GblTestScenario_enqueueSuite(pScenario,
+                                 GblTestSuite_create(GBL_STRING_VIEW_TEST_SUITE_CPP_TYPE));
+    GblTestScenario_enqueueSuite(pScenario,
                                  GblTestSuite_create(GBL_VERSION_TEST_SUITE_CPP_TYPE));
 #endif
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_RANDOM_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_COMPRESSION_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_EXCEPTION_TEST_SUITE_TYPE));
-    GblTestScenario_enqueueSuite(pScenario,
-                                 GblTestSuite_create(GBL_ERROR_TEST_SUITE_TYPE));
 
     GBL_CTX_END();
 }
@@ -279,7 +285,7 @@ GBL_EXPORT GblType GimbalTests_type(void) {
     static GblType type = GBL_INVALID_TYPE;
     static GblTypeInfo info = {
         .pFnClassInit = GimbalTestsClass_init_,
-        .classSize    = sizeof(GblTestScenarioClass),
+        .classSize    = sizeof(GimbalTestsClass),
         .instanceSize = sizeof(GimbalTests),
     };
     if GBL_UNLIKELY(type == GBL_INVALID_TYPE) {
